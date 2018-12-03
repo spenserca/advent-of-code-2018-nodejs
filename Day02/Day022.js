fc1c84bfaf8b9ccba7c4fb9d172b8cf1ad3304de
Spenser Andreassen <spenserca@gmail.com>
2018-12-03 17:15:57 -0600

    linting

--- Day02/Day022.js
+++ Day02/Day022.js
@@ -2,26 +2,27 @@ module.exports = (input) => {
   const boxIds = input.split('\n')
     .filter((i) => i)
     .reduce((accumulator, currentValue, currentIndex, sourceArray) => {
+      let matchingChars = accumulator;
 
       for (let i = currentIndex + 1; i < sourceArray.length; i++) {
         const sourceValue = sourceArray[i];
-        let differingIndexes = [];
+        let differingIndices = [];
 
         for (let y = 0; y < currentValue.length; y++) {
           if (currentValue[y] !== sourceValue[y]) {
-            differingIndexes.push(y);
+            differingIndices.push(y);
           }
         }
 
-        if (differingIndexes.length === 1) {
-          const start = currentValue.substring(0, differingIndexes[0]);
-          const end = currentValue.substring(differingIndexes[0] + 1);
-          accumulator = start + end;
-          // accumulator.push(currentValue.split('').splice(differingIndexes[0], 1).join(''));
+        if (differingIndices.length === 1) {
+          const start = currentValue.substring(0, differingIndices[0]);
+          const end = currentValue.substring(differingIndices[0] + 1);
+
+          matchingChars = start + end;
         }
       }
 
-      return accumulator;
+      return matchingChars;
     }, '');
 
   return boxIds;
