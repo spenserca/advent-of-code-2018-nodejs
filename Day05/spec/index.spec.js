8372dc59467bfa8f82007fda6c579d11f1a09d75
Spenser Andreassen <spenserca@gmail.com>
2018-12-09 09:30:01 -0600

    update index test

--- Day05/spec/index.spec.js
+++ Day05/spec/index.spec.js
@@ -2,6 +2,6 @@ const index = require('../index');
 
 describe('Day05/index', () => {
   it('should return the correct solution', () => {
-    expect(index).toEqual('Day051: 11364.');
+    expect(index).toEqual('Day051: 11364. Day052: 4212.');
   });
 });
