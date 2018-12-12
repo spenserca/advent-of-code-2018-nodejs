c734e9469d103ff48dcdc87f43fa1361d6f9f090
Andreassen <spencer.andreassen@PIONEER.COM>
2018-12-12 07:11:23 -0600

    more initialization

---
+++ Day07/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const day071 = require('./Day071');
+
+const input = fs.readFileSync('./Day07/input.txt', 'utf8');
+
+const solution = `Day071: ${day071(input)}.`;
+
+console.log(solution);
+
+module.exports = solution;
---
+++ Day07/spec/index.spec.js
@@ -0,0 +1,7 @@
+const index = require('../index');
+
+describe('Day07/index', () => {
+  it('should return the correct solution', () => {
+    expect(index).not.toBeUndefined();
+  });
+});
