c630a538fedc621b9fd4611a2420cbe4c7f4fe4a
Spenser Andreassen <spenserca@gmail.com>
2018-12-11 21:06:54 -0600

    cleaned up the code a little

--- Day06/Day061.js
+++ Day06/Day061.js
@@ -2,7 +2,7 @@ const parseCoordinates = (coordinateString, index) => {
   const coordinateValues = coordinateString.match(/([0-9]{1,})/g);
 
   return {
-    id: index,
+    id: `${coordinateValues[0]},${coordinateValues[1]}`,
     x: parseInt(coordinateValues[0]),
     y: parseInt(coordinateValues[1])
   };
@@ -12,7 +12,7 @@ const calculateDistances = (x, y, coordinates) =>
   coordinates.map((coordinate, index) => {
     return {
       distance: Math.abs(x - coordinate.x) + Math.abs(y - coordinate.y),
-      id: index
+      id: coordinate.id
     };
   });
 
@@ -26,36 +26,25 @@ module.exports = (input) => {
   const maxY = Math.max(...coordinates.map((coordinate) => coordinate.y));
   const minY = Math.min(...coordinates.map((coordinate) => coordinate.y));
 
-  // const filteredCoordinates = coordinates
-  //   .filter((coordinate) => !((coordinate.x === maxX && coordinate.y === maxY)
-  //     || (coordinate.x === minX && coordinate.y == maxY)
-  //     || (coordinate.x === maxX && coordinate.y === minY)
-  //     || (coordinate.x === minX && coordinate.y === minY)));
+  let areas = {};
 
-  let counts = {};
-
-  for (let x = minX - 2; x < maxX + 2; x++) {
-    for (let y = minY - 2; y < maxY + 2; y++) {
-      if (x < minX || x > maxX || y < minY || y > maxY) continue;
-
+  for (let x = minX; x < maxX; x++) {
+    for (let y = minY; y < maxY; y++) {
       const distances = calculateDistances(x, y, coordinates);
       const closestDistance = Math.min(...distances.map((distance) => distance.distance));
       const closestIds = distances.filter((distance) => distance.distance === closestDistance)
         .map((closest) => closest.id);
 
       if (closestIds.length === 1) {
-        if (counts[closestIds[0]]) {
-          counts[closestIds[0]]++;
-        } else {
-          counts[closestIds[0]] = 1;
+        if (areas[closestIds[0]]) {
+          areas[closestIds[0]]++;
+        }
+        else {
+          areas[closestIds[0]] = 1;
         }
       }
     }
   }
 
-  return Math.max(...Object.values(counts));
-  // return JSON.stringify(counts);
+  return Math.max(...Object.values(areas));
 };
-
-// That's not the right answer; your answer is too high. If you're stuck, there are some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 12568.) 
-// too high: 5041
--- Day06/spec/Day061.spec.js
+++ Day06/spec/Day061.spec.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const day061 = require('../Day061');
 
 describe('Day061', () => {
@@ -5,5 +6,11 @@ describe('Day061', () => {
     const input = '1, 1\r\n1, 6\r\n8, 3\r\n3, 4\r\n5, 5\r\n8, 9';
 
     expect(day061(input)).toEqual(17);
+  });
+
+  it('should return 3358 for the given input', () => {
+    const input = fs.readFileSync('./Day06/input.txt', 'utf8');
+
+    expect(day061(input)).toEqual(3358);
   });
 });
