3b59625eb0d32da9aa9b7463be853aa3039faaa7
Andreassen <spencer.andreassen@PIONEER.COM>
2018-12-12 08:36:35 -0600

    populating dependencies

--- Day07/Day071.js
+++ Day07/Day071.js
@@ -1,4 +1,26 @@
+const parseInstructions = (instruction) => {
+  const instructionSplit = instruction.split(' ');
+
+  return {
+    id: instructionSplit[1],
+    dependent: instructionSplit[7]
+  };
+};
+
 module.exports = (input) => {
   return input.split('\n')
-    .filter((i) => i);
+    .filter((i) => i)
+    .map(parseInstructions)
+    .reduce((accumulator, current) => {
+      if (accumulator[current.id]) {
+        accumulator[current.id].dependents.push(current.dependent);
+      } else {
+        console.log(`else: ${current.id} ${JSON.stringify(current)}`);
+        accumulator[current.id] = {
+          dependents: [current.dependent]
+        };
+      }
+
+      return accumulator;
+    }, {});
 };
--- Day07/index.js
+++ Day07/index.js
@@ -3,7 +3,7 @@ const day071 = require('./Day071');
 
 const input = fs.readFileSync('./Day07/input.txt', 'utf8');
 
-const solution = `Day071: ${day071(input)}.`;
+const solution = `Day071: ${JSON.stringify(day071(input))}.`;
 
 console.log(solution);
 
